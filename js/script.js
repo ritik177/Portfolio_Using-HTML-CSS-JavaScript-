9efdf3f3a140bc651ba71fb3a41f6defb4ed3b05
Ritik Patel <ritikpatel82993@gmail.com>
2024-01-06 16:18:58 +0530

    third commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =================typing animation ============== */
+var typed = new Typed(".typing", {
+    strings: ["", "Web Developer", "web Designer", "FronEnd Developer", "MERN Stack Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
